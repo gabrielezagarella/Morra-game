d4a5499601b1c303dd99a3ab365479e2be9fb80e
gabrielezagarella <gabry-zaga20@hotmail.it>
2020-03-16 19:50:57 +0100

    fixed bug

--- Morra/src/morra/Morra.kt
+++ Morra/src/morra/Morra.kt
@@ -10,22 +10,32 @@ open class Morra (){
     open var arg:String = " "
 
     fun morra(): String{
-        val casual = arrayOf("paper", "scissor", "stone")
-        val randomIndex = Math.random()* casual.count()
-        val randomElement = casual[randomIndex.toInt()]
+        try {
+            if (arg != "paper" && arg != "scissor" && arg != "stone") {
+                return " "
+            }
+            val casual = arrayOf("paper", "scissor", "stone")
+            val randomIndex = Math.random()* casual.count()
+            val randomElement = casual[randomIndex.toInt()]
 
-        if (arg == randomElement){
-            return "Result: You draw! Player: ${arg}, Computer: ${randomElement}"
+            if (arg == randomElement){
+                return "Result: You draw! Player: ${arg}, Computer: ${randomElement}"
+            }
+            else if (arg == "paper" && randomElement == "stone" ||
+                arg == "stone" && randomElement == "scissor" ||
+                arg == "scissor" && randomElement == "paper"){
+                return "Result: You win! Player: ${arg}, Computer: ${randomElement}"
+            }
+            else
+                return "Result: You lose! Player: ${arg}, Computer: ${randomElement}"
         }
-        else if (arg == "paper" && randomElement == "stone" ||
-            arg == "stone" && randomElement == "scissor" ||
-            arg == "scissor" && randomElement == "paper"){
-            return "Result: You win! Player: ${arg}, Computer: ${randomElement}"
+        catch (e: NullPointerException) {
+            throw Error("Error")
         }
-        else
-            return "Result: You lose! Player: ${arg}, Computer: ${randomElement}"
     }
 }
+
+
 
 
 
